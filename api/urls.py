fac6814ac04e25f6e09afa434f42c320d8c72ad2
Pedro Rodrigues Lima <perliima72@gmail.com>
2017-04-24 16:13:09 -0300

    final commit

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.authtoken.views import obtain_auth_token
 from .views import CreateView, DetailsView
 
 urlpatterns = {
@@ -7,6 +8,7 @@ urlpatterns = {
     url(r'^bucketlists/$', CreateView.as_view(), name='create'),
     url(r'^bucketlists/(?P<pk>[0-9]+)/$',
         DetailsView.as_view(), name='details'),
+    url(r'get-token/', obtain_auth_token)
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
