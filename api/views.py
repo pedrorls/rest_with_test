cffc9f22f86d7462f499db6cc17f0c044f66255c
Pedro Rodrigues Lima <perliima72@gmail.com>
2017-04-24 00:41:29 -0300

    class created to RETRIEVE DELETE UPDATE

--- api/views.py
+++ api/views.py
@@ -10,3 +10,8 @@ class CreateView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save()
+
+
+class DetailsView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
